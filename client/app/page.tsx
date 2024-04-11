b1fe49f34fb6fc32da249200d7ae7caa0b73e75b
Kevin <113942748+kevy-kev@users.noreply.github.com>
2024-04-11 14:28:33 -0400

    chore: remove style guide eslint plugin

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from 'react'
 
-export default function Home (): ReactNode {
+export default function Home () {
   return (
     <main>
       <h1>Legend Mama: Under Construction</h1>
