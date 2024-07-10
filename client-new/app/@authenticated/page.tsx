0754ad9226645b8794e21bf9fb7abd085f2c8c7d
Megan Wooley <wooleymegan@gmail.com>
2024-07-10 17:09:32 -0500

    Updating UI theme - Working version with structure in place

--- client-new/app/@authenticated/page.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import DashboardMain from "@/app/@authenticated/components/dashboard-main";
-
-export default function Dashboard() {
-  return (
-    <DashboardMain/>
-  );
-}
